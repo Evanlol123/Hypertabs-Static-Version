e3a582ef5f4d0443d0a2263be585c39517f4efb6
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-10-15 01:01:09 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 	prefix: '/uv/service/',
-	bare: `https://hcpss-me.vercel.app/v/`,
+	bare: `https://ttt.jdx3.org/`,
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: '/uv/uv.handler.js',
